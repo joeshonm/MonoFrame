25a53cd9ea2c0a432cdb18d3f2490d567a0c888e
JoeShon Monroe <me@joeshonm.com>
2023-11-12 00:24:04 -0500

    initial commit

--- Sources/MonoFrame/MonoFrame.swift
+++ Sources/MonoFrame/MonoFrame.swift
@@ -1,2 +1,10 @@
-// The Swift Programming Language
+// MonoFrame - UI Kit for SwiftUI
 // https://docs.swift.org/swift-book
+
+@available(macOS 10.15, *)
+public class MonoFrame {
+
+    init(_ configuration: MonoFrameConfiguration) {
+        print(configuration.primaryColor)
+    }
+}
---
+++ Sources/MonoFrame/MonoFrameConfiguration.swift
@@ -0,0 +1,13 @@
+//
+//  MonoFrameConfiguration.swift
+//
+//
+//  Created by JoeShon Monroe on 11/11/23.
+//
+
+import SwiftUI
+
+@available(macOS 10.15, *)
+public struct MonoFrameConfiguration {
+    var primaryColor: Color = .black
+}
